e0c8f6da535936722fdd05c14dad9f4b17d707de
MukeshAppana <appanamukesh77@gmail.com>
2022-09-26 04:52:48 +0000

    faqsapp

--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,40 +1,22 @@
-import {Component} from 'react'
-
 import FaqItem from '../FaqItem'
 
 import './index.css'
 
-class Faqs extends Component {
-  state = {
-    isActive: false,
-    activeId: null,
-  }
+const Faqs = props => {
+  const {faqsList} = props
 
-  clickBtn = id => {
-    this.setState(prevState => ({activeId: id, isActive: !prevState.isActive}))
-  }
-
-  render() {
-    const {faqsList} = this.props
-    const {activeId} = this.state
-    return (
-      <div className="main-container">
-        <div className="inner-container">
-          <h1 className="main-heading">FAQs</h1>
-          <ul className="ul-list">
-            {faqsList.map(eachValue => (
-              <FaqItem
-                listItem={eachValue}
-                key={eachValue.id}
-                state={this.state}
-                activeId={activeId}
-                clickBtn={this.clickBtn}
-              />
-            ))}
-          </ul>
-        </div>
+  return (
+    <div className="app-container">
+      <div className="faqs-container">
+        <h1 className="heading">FAQs</h1>
+        <ul className="faqs-list">
+          {faqsList.map(eachFaq => (
+            <FaqItem key={eachFaq.id} faqDetails={eachFaq} />
+          ))}
+        </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
 export default Faqs
